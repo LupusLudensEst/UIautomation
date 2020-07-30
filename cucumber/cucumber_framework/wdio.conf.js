d2024469b2b98c3df2069b6be176056c213b05dd
mchorney <m.chorney7@gmail.com>
2020-07-30 15:42:01 -0500

    Update wdio.conf.js
    Comment base URL on line 75

--- cucumber/cucumber_framework/wdio.conf.js
+++ cucumber/cucumber_framework/wdio.conf.js
@@ -72,7 +72,7 @@ exports.config = {
     //
     // Set a base URL in order to shorten url command calls. If your url
     // parameter starts with "/", then the base url gets prepended.
-    baseUrl: 'http://deens-master.now.sh',
+    //baseUrl: 'http://deens-master.now.sh',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
